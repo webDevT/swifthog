6b1d1504d1165c891bbe27e6f8de31e77bbe15ff
karinadesyatnik <karinadesyatnik27@gmail.com>
2020-10-09 17:17:14 +0300

    register and login

--- app/js/main.js
+++ app/js/main.js
@@ -49,5 +49,75 @@ $('.load-more-button').click(function(){
 });
 //end load-more
 
+$('body').on('click', '.password-control', function(){
+	if ($('#password-input').attr('type') == 'password'){
+		$(this).addClass('view');
+		$('#password-input').attr('type', 'text');
+	} else {
+		$(this).removeClass('view');
+		$('#password-input').attr('type', 'password');
+	}
+	return false;
+});
+
+
+$('.sign-in, .sign-in-mobile').click(function(){
+	
+	$(".popup-sign-in").fadeIn();
+	$(".popup-sign-in").addClass('active');
+});
+
+$('.button--register, .login-mobile, .sign-up__btn').click(function(){
+	
+	$(".popup-register").fadeIn();
+	$(".popup-register").addClass('active');
+});
+
+$('.sign-up__btn').click(function(){
+	
+	$(".popup-register").fadeIn();
+	$(".popup-register").addClass('active');
+	$(".popup-sign-in").fadeOut();
+	$(".popup-sign-in").removeClass('active');
+});
+
+$('.sign-in__btn').click(function(){
+	
+	$(".popup-register").fadeOut();
+	$(".popup-register").removeClass('active');
+	$(".popup-sign-in").fadeIn();
+	$(".popup-sign-in").addClass('active');
+});
+
+$('.close').click(function(){
+	
+	$(".popup").fadeOut();
+	$(".popup").removeClass('active');
+});
 
 });
+
+jQuery(function($){
+  $(document).mouseup(function (e){ 
+    var searchWrapper = $(".popup-content"); 
+     var searchRow = $(".popup"); 
+    if (!searchWrapper.is(e.target) 
+        && searchWrapper.has(e.target).length === 0) { 
+      searchRow.removeClass('active'); 
+  	searchRow.fadeOut(); 
+    }
+  });
+});
+
+
+
+
+
+
+$('.header__menu li').click(function(){
+	$(".header__menu").fadeOut();
+	$(".menu-btn").removeClass('active');
+});
+
+
+
